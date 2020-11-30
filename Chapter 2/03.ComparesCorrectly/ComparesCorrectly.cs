2c16a224079a8b3f26b8b350f62df21694e51147
Toufik Boukraa <toufikboukraa@gmail.com>
2020-11-30 20:59:47 +0100

    Finiched the problem 03.ComparesCorrectly.

--- Chapter 2/03.ComparesCorrectly/ComparesCorrectly.cs
+++ Chapter 2/03.ComparesCorrectly/ComparesCorrectly.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+// Write a program, which compares correctly two real numbers with accuracy at least 0.000001
+
+using System;
 
 namespace ComparesCorrectly
 {
@@ -10,37 +9,20 @@ namespace ComparesCorrectly
     {
         static void Main(string[] args)
         {
-            /* private static bool AreEqualWithPrecision(decimal a, decimal b, decimal precision)
+            Console.Write("Enter first number: ");
+            string m = Console.ReadLine();
+            double firstNumber = double.Parse(m);
+            Console.Write("Enter second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+            bool equal = Math.Abs(firstNumber - secondNumber) < 0.000001;
+            if (equal == true)
             {
-                return Math.Abs(a - b) < precision;
-            }*/
-            
-            
-            try
-            {
-                Console.Write("Enter first number: ");
-                string m = Console.ReadLine();
-                double firstNumber = double.Parse(m);
-                Console.Write("Enter second number: ");
-                double secondNumber = double.Parse(Console.ReadLine());
-                bool equal = Math.Abs(firstNumber - secondNumber) < 0.000001;
-                if (equal == true)
-                {
-                    Console.WriteLine("The numbers are equal.");
-                }
-                else
-                {
-                    Console.WriteLine("The numbers are not equal.");
-                }
+                Console.WriteLine("The numbers are equal.");
             }
-            catch (Exception)
+            else
             {
-
-                throw;
+                Console.WriteLine("The numbers are not equal.");
             }
-            
-            
         }
-
-}
+    }
 }
