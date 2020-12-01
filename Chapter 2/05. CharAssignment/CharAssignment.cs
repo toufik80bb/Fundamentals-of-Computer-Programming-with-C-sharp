c98c12d6ac60a72a1d375823a7c294cc50655e42
toufik80bb <toufikboukraa@gmail.com>
2020-12-01 15:22:30 -0800

    Finiched the problem 05.CharAssignment.

---
+++ Chapter 2/05. CharAssignment/CharAssignment.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+
+
+namespace CharAssignment
+{
+	class CharAssignment
+	{
+		static void Main()
+		{
+			// Hexadessimal representation of 72 is 48.
+			char c = '\u0048';
+			Console.WriteLine(c);
+			Console.ReadLine();
+		}
+	}
+}
