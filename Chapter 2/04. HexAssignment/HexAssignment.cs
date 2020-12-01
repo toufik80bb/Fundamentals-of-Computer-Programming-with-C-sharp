0b88daabdd5c8064eb5d5874d22e7215cfc2ef8b
toufik80bb <toufikboukraa@gmail.com>
2020-12-01 14:27:53 -0800

    Finiched the problem 04.HexAssignment .

---
+++ Chapter 2/04. HexAssignment/HexAssignment.cs
@@ -0,0 +1,16 @@
+﻿// Initialize a variable of type int with a value of 256 in hexadecimal format (256 is 100 in a numeral system with base 16).
+
+using System;
+
+namespace HexAssignment
+{
+	class HexAssignment
+	{
+		static void Main()
+		{
+			int HexValue = 0x100;
+			Console.WriteLine(HexValue);
+			Console.ReadLine();
+		}
+	}
+}
