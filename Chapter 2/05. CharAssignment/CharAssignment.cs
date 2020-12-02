a69268312bc565347d35b3c88b3521f2e5cfe5b4
toufik80bb <toufikboukraa@gmail.com>
2020-12-01 16:12:04 -0800

    Finiched problem 06.BoolAssignment.

--- Chapter 2/05. CharAssignment/CharAssignment.cs
+++ Chapter 2/05. CharAssignment/CharAssignment.cs
@@ -1,7 +1,7 @@
-﻿
+﻿/* Declare a variable of type char and assign it as a value the character, which has Unicode code, 72
+ * (use the Windows calculator in order to find hexadecimal representation of 72).*/
 
 using System;
-
 
 namespace CharAssignment
 {
---
+++ Chapter 2/06. BoolAssignment/BoolAssignment.cs
@@ -0,0 +1,27 @@
+﻿
+//Declare a variable isMale of type bool and assign a value to it depending on your gender.
+
+using System;
+
+namespace BoolAssignment
+{
+	class BoolAssignment
+	{
+		static void Main()
+		{
+			Console.WriteLine("Enter your gender please! Male or Female. ");
+			string gender = Console.ReadLine();
+			bool isMale;
+			if (gender == "Male")
+			{
+				isMale = true;
+			}
+			else
+			{
+				isMale = false;
+			}
+
+			Console.WriteLine("So you are a {0}.", isMale ? "Man" : "Woman");
+		}
+	}
+}
