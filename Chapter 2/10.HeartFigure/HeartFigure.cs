e5e67268c6063a8f54f76c9b9d38f198a5a626e5
toufik80bb <toufikboukraa@gmail.com>
2020-12-14 10:52:39 -0800

    Submit solution of the problem #10.

---
+++ Chapter 2/10.HeartFigure/HeartFigure.cs
@@ -0,0 +1,20 @@
+﻿/*Write a program to print a figure in the shape of a heart by the sign "o".*/
+using System;
+
+namespace HeartFigure
+{
+	class HeartFigure
+	{
+		static void Main()
+		{
+
+			Console.WriteLine(" 0     0 ");
+			Console.WriteLine("0 0   0 0");
+			Console.WriteLine("0   0   0");
+			Console.WriteLine(" 0     0 ");
+			Console.WriteLine("   0 0   ");
+			Console.WriteLine("    0    ");
+			Console.WriteLine();
+		}
+	}
+}
