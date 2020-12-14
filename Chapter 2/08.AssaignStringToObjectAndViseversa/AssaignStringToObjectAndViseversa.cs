5c90916674a82b7e785e4fe450c674d2784b7917
toufik80bb <toufikboukraa@gmail.com>
2020-12-14 09:50:14 -0800

    Submit solution to the 8 problem.

--- Chapter 2/08.AssaignStringToObjectAndViseversa/AssaignStringToObjectAndViseversa.cs
+++ Chapter 2/08.AssaignStringToObjectAndViseversa/AssaignStringToObjectAndViseversa.cs
@@ -1,6 +1,7 @@
 ﻿/* Declare two variables of type string and give them values "Hello" and "World".
- * Assign the value obtained by the concatenation of the two variables of type string (do not miss the space in the middle) to a variable of type object.
- * Declare a third variable of type string and initialize it with the value of the variable of type object (you should use type casting).*/
+ * Assign the value obtained by the concatenation of the two variables of type string (do not miss the space in the middle)
+ * to a variable of type object. Declare a third variable of type string and initialize it with the value of the variable of type object
+ * (you should use type casting).*/
 
 using System;
 
@@ -13,8 +14,8 @@ namespace AssaignStringToObjectAndViseversa
             string hello = "Hello";
             string world = "World";
             object obj = hello + " " + world;
-
-            Console.WriteLine(obj);
+            string str = (string)obj;
+            Console.WriteLine(str);
         }
     }
 }
