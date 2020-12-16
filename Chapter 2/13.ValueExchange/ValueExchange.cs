7a5b2d63878053604bc9d0bbd1870968e00855d9
toufik80bb <toufikboukraa@gmail.com>
2020-12-16 15:56:48 -0800

    Submit solution to problem #13.

---
+++ Chapter 2/13.ValueExchange/ValueExchange.cs
@@ -0,0 +1,20 @@
+﻿/*Declare two variables of type int. Assign to them values 5 and 10 respectively.Exchange(swap) their values and print them.*/
+
+using System;
+
+
+namespace ValueExchange
+{
+	class ValueExchange
+	{
+		static void Main()
+		{
+			int a = 5;
+			int b = 10;
+			int c = b;
+			b = a;
+			b = c;
+			Console.WriteLine("a =  " + a + "    " + "b =  " + b);
+		}
+	}
+}
